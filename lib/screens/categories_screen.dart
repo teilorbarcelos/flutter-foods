bde0ad73a7b04a1a1714bb89d26ad97f18af669b
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2023-01-10 22:45:25 -0300

    categories mock and category model and screen created

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [],
+    );
+  }
+}
