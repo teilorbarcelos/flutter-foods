9fb04ebe11e617b135f255c69731adaa54cfbeb2
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2023-01-23 11:18:25 -0300

    food detail screen ingredients style done

--- lib/screens/food_detail_screen.dart
+++ lib/screens/food_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foods/models/food.dart';
+
 class FoodDetailScreen extends StatelessWidget {
   const FoodDetailScreen({super.key});
 
@@ -10,8 +11,54 @@ class FoodDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(food.title),
       ),
-      body: Center(
-        child: Text('Detalhes da Refeição'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              food.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: ListView.builder(
+              itemCount: food.ingredients.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(food.ingredients[index]),
+                  ),
+                  color: Theme.of(context).colorScheme.secondary,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
