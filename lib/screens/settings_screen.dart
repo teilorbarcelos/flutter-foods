573a33d1453ffb1c49fd98043cea3bc844f10546
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2023-02-16 23:19:40 -0300

    config screen switches added

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,9 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_foods/components/main_drawer.dart';
+import 'package:flutter_foods/models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    void Function(bool) onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChanged,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +35,46 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Configurações'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Configurações!'),
-      )
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              _createSwitch(
+                'Sem glúten',
+                'Só exibe refeições sem glúten!',
+                settings.isGlutenFree,
+                (value) => setState(() => settings.isGlutenFree = value),
+              ),
+              _createSwitch(
+                'Sem Lactose',
+                'Só exibe refeições sem Lactose!',
+                settings.isLactoseFree,
+                (value) => setState(() => settings.isLactoseFree = value),
+              ),
+              _createSwitch(
+                'Vegana',
+                'Só exibe refeições Veganas!',
+                settings.isVegan,
+                (value) => setState(() => settings.isVegan = value),
+              ),
+              _createSwitch(
+                'Vegetariana',
+                'Só exibe refeições Vegetarianas!',
+                settings.isVegetarian,
+                (value) => setState(() => settings.isVegetarian = value),
+              ),
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
